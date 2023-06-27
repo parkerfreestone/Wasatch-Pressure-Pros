ff0109c236cffaf3c51507ba328acb271aea58c3
Parker Freestone <parkerfreestone@gmail.com>
2023-06-27 00:15:43 -0600

    :sparkles: Added some additional styling...

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,49 +3,51 @@ import BannerVideo from "../assets/power-washing-banner.mp4";
 
 export const Hero = () => {
   return (
-    <section className="relative mx-4 sm:mx-16 overflow-hidden mt-4">
-      <video
-        className="absolute top-0 left-0 w-full h-full object-cover rounded-lg"
-        src={BannerVideo}
-        autoPlay
-        muted
-        loop
-      />
-      <div className="absolute top-0 left-0 w-full h-full bg-black/60 rounded-lg" />
-      <div className="relative z-10mx-auto px-6 sm:px-8 lg:px-10 xl:px-20 py-12 sm:py-52">
-        <p className="flex items-center gap-2 text-base sm:text-xl text-white font-bold">
-          <div>
-            <Droplet size={16} />
+    <section className="relative pb-24 rounded-b-3xl z-30 bg-white">
+      <div className="relative mx-4 sm:mx-16 overflow-hidden mt-4">
+        <video
+          className="absolute top-0 left-0 w-full h-full object-cover rounded-3xl"
+          src={BannerVideo}
+          autoPlay
+          muted
+          loop
+        />
+        <div className="absolute top-0 left-0 w-full h-full bg-black/60 rounded-3xl" />
+        <div className="relative z-10mx-auto px-6 sm:px-8 lg:px-10 xl:px-20 py-12 sm:py-52">
+          <p className="flex items-center gap-2 text-base sm:text-xl text-white font-bold">
+            <div>
+              <Droplet size={16} />
+            </div>
+            Precision in Power Washing
+          </p>
+          <h1 className="text-4xl sm:text-8xl text-white font-extrabold mt-6 sm:mt-12">
+            <span className="inline sm:block">Professional </span>
+            <span>Power Washing Services</span>
+          </h1>
+          <p className="mt-2 sm:mt-4 text-sm sm:text-xl text-white">
+            <span className="inline sm:block">
+              Experience the transformation with our expert power washing
+              services.
+            </span>
+            <span>We deliver superior quality,</span> enhancing the beauty and
+            value of your property.
+          </p>
+          <div className="flex flex-col sm:flex-row gap-4 mt-6 sm:mt-24">
+            <a
+              href="#"
+              className="px-4 sm:px-8 py-2 sm:py-3 bg-teal-500 rounded-full flex items-center justify-center gap-2 font-bold text-white"
+            >
+              CONTACT US
+              <ArrowUpRight />
+            </a>
+            <a
+              href="#"
+              className="px-4 sm:px-8 py-2 sm:py-3 rounded-full border border-white flex items-center justify-center gap-2 font-bold text-white"
+            >
+              LEARN MORE
+              <ArrowRight />
+            </a>
           </div>
-          Precision in Power Washing
-        </p>
-        <h1 className="text-4xl sm:text-8xl text-white font-extrabold mt-6 sm:mt-12">
-          <span className="inline sm:block">Professional </span>
-          <span>Power Washing Services</span>
-        </h1>
-        <p className="mt-2 sm:mt-4 text-sm sm:text-xl text-white">
-          <span className="inline sm:block">
-            Experience the transformation with our expert power washing
-            services.
-          </span>
-          <span>We deliver superior quality,</span> enhancing the beauty and
-          value of your property.
-        </p>
-        <div className="flex flex-col sm:flex-row gap-4 mt-6 sm:mt-24">
-          <a
-            href="#"
-            className="px-4 sm:px-8 py-2 sm:py-3 bg-teal-500 rounded-full flex items-center justify-center gap-2 font-bold text-white"
-          >
-            CONTACT US
-            <ArrowUpRight />
-          </a>
-          <a
-            href="#"
-            className="px-4 sm:px-8 py-2 sm:py-3 rounded-full border border-white flex items-center justify-center gap-2 font-bold text-white"
-          >
-            LEARN MORE
-            <ArrowRight />
-          </a>
         </div>
       </div>
     </section>
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -2,41 +2,43 @@ import { Building, HelpCircle, Home } from "lucide-react";
 
 export const Services = () => {
   return (
-    <div className="flex gap-6 justify-between mx-16 mt-12">
-      <div className="flex-1 p-6 border border-zinc-800 rounded-xl">
-        <Building />
-        <h2 className="text-3xl text-zinc-850 font-bold mb-8 mt-4">
-          Commercial
-        </h2>
-        <p className="text-xl text-zinc-600">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-          debitis ratione a quis in. Cupiditate architecto ea, sunt impedit quis
-          laboriosam itaque sint, explicabo consectetur totam distinctio,
-          voluptatibus quae corporis!
-        </p>
+    <section className="relative bg-zinc-300 rounded-b-3xl -mt-24 pt-24 pb-12 -z-10">
+      <div className="flex gap-6 justify-between mx-16 mt-12">
+        <div className="flex-1 p-6 border border-zinc-800 rounded-xl">
+          <Building className="text-zinc-8000" />
+          <h2 className="text-3xl text-zinc-800 font-bold mb-8 mt-4">
+            Commercial
+          </h2>
+          <p className="text-xl text-zinc-700">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+            debitis ratione a quis in. Cupiditate architecto ea, sunt impedit
+            quis laboriosam itaque sint, explicabo consectetur totam distinctio,
+            voluptatibus quae corporis!
+          </p>
+        </div>
+        <div className="flex-1 p-6 border border-zinc-800 rounded-xl">
+          <Home className="text-zinc-8000" />
+          <h2 className="text-3xl text-zinc-800 font-bold mb-8 mt-4">
+            Residential
+          </h2>
+          <p className="text-xl text-zinc-700">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+            debitis ratione a quis in. Cupiditate architecto ea, sunt impedit
+            quis laboriosam itaque sint, explicabo consectetur totam distinctio,
+            voluptatibus quae corporis!
+          </p>
+        </div>
+        <div className="flex-1 p-6 border border-zinc-800 rounded-xl">
+          <HelpCircle className="text-zinc-8000" />
+          <h2 className="text-3xl text-zinc-800 font-bold mb-8 mt-4">Custom</h2>
+          <p className="text-xl text-zinc-700">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+            debitis ratione a quis in. Cupiditate architecto ea, sunt impedit
+            quis laboriosam itaque sint, explicabo consectetur totam distinctio,
+            voluptatibus quae corporis!
+          </p>
+        </div>
       </div>
-      <div className="flex-1 p-6 border border-zinc-800 rounded-xl">
-        <Home />
-        <h2 className="text-3xl text-zinc-850 font-bold mb-8 mt-4">
-          Residential
-        </h2>
-        <p className="text-xl text-zinc-600">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-          debitis ratione a quis in. Cupiditate architecto ea, sunt impedit quis
-          laboriosam itaque sint, explicabo consectetur totam distinctio,
-          voluptatibus quae corporis!
-        </p>
-      </div>
-      <div className="flex-1 p-6 border border-zinc-800 rounded-xl">
-        <HelpCircle />
-        <h2 className="text-3xl text-zinc-850 font-bold mb-8 mt-4">Custom</h2>
-        <p className="text-xl text-zinc-600">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-          debitis ratione a quis in. Cupiditate architecto ea, sunt impedit quis
-          laboriosam itaque sint, explicabo consectetur totam distinctio,
-          voluptatibus quae corporis!
-        </p>
-      </div>
-    </div>
+    </section>
   );
 };
