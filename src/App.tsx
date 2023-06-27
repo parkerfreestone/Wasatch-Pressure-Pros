12f9eb40e27d1c098d418418381e975f46a3f733
Parker Freestone <parkerfreestone@gmail.com>
2023-06-26 23:55:57 -0600

    :sparkles: Added better SEO with react helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet";
 import { Hero } from "./components/Hero";
 import { Nav } from "./components/Nav";
 import { Services } from "./components/Services";
@@ -5,6 +6,19 @@ import { Services } from "./components/Services";
 function App() {
   return (
     <>
+      <Helmet>
+        <title>
+          Wasatch Power Pros: Professional Power Washing Services in Utah
+        </title>
+        <meta
+          name="description"
+          content="Wasatch Power Pros provides top-notch power washing services in Utah. Enhance the beauty and value of your property with our superior cleaning solutions"
+        />
+        <meta
+          name="keywords"
+          content="Utah Power Washing, Utah Pressure Washing, Power Washing, Wasatch Power Pros, Professional Cleaning, Commercial Power Washing, Residential Power Washing, Power Washing Utah, Pressure, Pressure Washing"
+        />
+      </Helmet>
       <header>
         <Nav />
       </header>
